500e9cb317fcea19d739dae9cee2bc2d8addab65
Victor Hu <vihu@silabs.com>
2020-03-18 13:14:32 +0800

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 "# spider_salesforce" 
+
+Make sure you log into the saleforce and stop in the queue you want
+# Climbing web info
+* click F12 to enter chrome debug mode, choose NetWork tab and clean all log in window.
+* Fresh queue (Not F5)
+# Find cookies, token and loadID
+* After fresh, you will find a item in debug window, choose it. You will find all kinds of paramter in Header tab.
+* Please find 3 important parameter in headers.
+
+Please see the picture below to know how to get cookies.
+![cookies](./img/02.png)
+
+Please see the picture below to know how to get loadID and token.
+![token](./img/03.png)
+# Find Queue id, user id
+Queue id can be found in your link address, such as https://siliconlabs.lightning.force.com/lightning/o/Case/list?filterName=00B1M0000096zClUAI
+"00B1M0000096zClUAI" is the queue id.
+
+User id can be found it your user page.
+![token](./img/04.png)
+
+# Fill up paramter into python file
+Open webinfo_salesforce.py and fill up the 5 paramters above.
+
+# Add number to take_list
+Add the mantissa number into take_list, spider will pick these case and give to you.
+
+# Start run
+python start_spider
+
+# Note
+* If the spider run correctly, it will output the caputure result. When the case number in take_list and belong to APAC, the case will be assigned to you by spider.
+![token](./img/05.png)
+* Spider caputre the case info every 1 minute
+* We can log into salesforce since we feed it cookies(include token and loadID)  from chrome, the cookies has life cycle, about 2-3 days. That means you may need to feed them again after the cookies became invalid.
+
+Please contact if it can't work on your side
+
+Welcome add more feature to the code to make it more friendly.
--- start_spider.py
+++ start_spider.py
@@ -6,7 +6,7 @@ import sys
 import requests
 import json
 import time
-import cookies_salesforce
+import webinfo_salesforce
 from datetime import datetime
 
 #  https://siliconlabs.lightning.force.com/aura?r=5&ui-force-components-controllers-lists-listViewDataManager.ListViewDataManager.getItems=1&ui-force-components-controllers-lists-listViewManagerGrid.ListViewManagerGrid.getRecordLayoutComponent=1
@@ -16,15 +16,23 @@ from datetime import datetime
 # token = 'eyJub25jZSI6IkxJTVRaSVVzcm9hUTc1eVFDZ1QtZUdlajZQQkxRODlGSFZCUjBfLXZkem9cdTAwM2QiLCJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImtpZCI6IntcInRcIjpcIjAwRDE2MDAwMDAwTkZZQ1wiLFwidlwiOlwiMDJHMU0wMDAwMDBEOWxpXCIsXCJhXCI6XCJjYWltYW5zaWduZXJcIn0iLCJjcml0IjpbImlhdCJdLCJpYXQiOjE1ODQzMjI2NTgxMDEsImV4cCI6MH0=..m3SS31wWi8MXBpofGMTF8_NkMHVIQBY03Zpt4VxvGek='
 
 # loadID=''
+# queueID='00B1M000009ZmXSUA0'
+# userID='0051M000007zvqZQAQ'
+# take_list=['1','5']
 
-cookies, token,loadID = cookies_salesforce.get_login_parameters()
+cookies, token,loadID = webinfo_salesforce.get_login_parameters()
+
+queueID = webinfo_salesforce.get_queueID()
+userID = webinfo_salesforce.get_userID()
+
+take_list = webinfo_salesforce.get_take_list()
 
 # Do NOT touch the code below
 
 header = {
     "Host": "siliconlabs.lightning.force.com",
     "origin": "https://siliconlabs.lightning.force.com",
-    "Referer": "https://siliconlabs.lightning.force.com/lightning/o/Case/list?filterName=00B1M000009ZmXSUA0",
+    "Referer": "https://siliconlabs.lightning.force.com/lightning/o/Case/list?filterName="+queueID,
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36',
     'Cookie': cookies
 }
@@ -32,9 +40,9 @@ header = {
 postUrl = "https://siliconlabs.lightning.force.com/aura?r=62&ui-force-components-controllers-lists-listViewDataManager.ListViewDataManager.getItems=1&ui-force-components-controllers-lists-listViewManagerGrid.ListViewManagerGrid.getRecordLayoutComponent=1"
 
 postData = {
-    "message": '{"actions":[{"id":"51987;a","descriptor":"serviceComponent://ui.force.components.controllers.lists.listViewManagerGrid.ListViewManagerGridController/ACTION$getRecordLayoutComponent","callingDescriptor":"UNKNOWN","params":{"listReference":{"entityKeyPrefixOrApiName":"Case","listViewIdOrName":"00B1M000009ZmXSUA0","inContextOfRecordId":null,"isMRU":false,"isSearch":false},"layoutType":"LIST","layoutMode":"EDIT","inContextOfComponent":"force:listViewManagerGrid","enableMassActions":true,"enableRowErrors":true,"useHoversForLookup":false}},{"id":"51990;a","descriptor":"serviceComponent://ui.force.components.controllers.lists.listViewDataManager.ListViewDataManagerController/ACTION$getItems","callingDescriptor":"UNKNOWN","params":{"filterName":"00B1M000009ZmXSUA0","entityName":"Case","pageSize":50,"layoutType":"LIST","sortBy":null,"getCount":false,"enableRowActions":false,"offset":0},"storable":true}]}',
+    "message": '{"actions":[{"id":"51987;a","descriptor":"serviceComponent://ui.force.components.controllers.lists.listViewManagerGrid.ListViewManagerGridController/ACTION$getRecordLayoutComponent","callingDescriptor":"UNKNOWN","params":{"listReference":{"entityKeyPrefixOrApiName":"Case","listViewIdOrName":"'+queueID+'","inContextOfRecordId":null,"isMRU":false,"isSearch":false},"layoutType":"LIST","layoutMode":"EDIT","inContextOfComponent":"force:listViewManagerGrid","enableMassActions":true,"enableRowErrors":true,"useHoversForLookup":false}},{"id":"51990;a","descriptor":"serviceComponent://ui.force.components.controllers.lists.listViewDataManager.ListViewDataManagerController/ACTION$getItems","callingDescriptor":"UNKNOWN","params":{"filterName":"'+queueID+'","entityName":"Case","pageSize":50,"layoutType":"LIST","sortBy":null,"getCount":false,"enableRowActions":false,"offset":0},"storable":true}]}',
     "aura.context": '{"mode":"PROD","fwuid":"kHqYrsGCjDhXliyGcYtIfA","app":"one:one","loaded":{"APPLICATION@markup://one:one":"'+loadID+'"},"dn":[],"globals":{"eswConfigDeveloperName":null,"isVoiceOver":null,"setupAppContextId":null,"density":"VIEW_TWO","srcdoc":null,"appContextId":"06m1M000000KhjGQAS","dynamicTypeSize":null},"uad":true}',
-    "aura.pageURI": '/lightning/o/Case/list?filterName=00B1M000009ZmXSUA0',
+    "aura.pageURI": '/lightning/o/Case/list?filterName='+queueID,
     "aura.token": token
 }
 
@@ -86,9 +94,9 @@ while True:
                     case_number =text2["context"]["globalValueProviders"][6]["values"]["records"][key]["Case"]["record"]["fields"]["CaseNumber"]["value"] # case number, type:string
                     case_district= text2["context"]["globalValueProviders"][6]["values"]["records"][key]["Case"]["record"]["fields"]["District__c"]["value"] # district
                     print str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')) + " query for " + str(i) + " time: find case " + str(case_number) +' '+ str(case_district)
-                    if (case_number[len(case_number)-1]=='5' or case_number[len(case_number)-1]=='1') and case_district=='APAC':
+                    if (case_number[len(case_number)-1] in take_list and case_district=='APAC':
                         print str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')) + "-------query for " + str(i) + " time: get case " + str(case_number) +' '+ str(case_district)
-                        message_data = '{"actions":[{"id":"56925;a","descriptor":"serviceComponent://ui.force.components.controllers.ownerChangeContent.OwnerChangeContentController/ACTION$performOwnerChange","callingDescriptor":"UNKNOWN","params":{"recordIds":["' + key + '"],"newOwnerId":"0051M000007zvqZQAQ","changeOwnerOptions":{"editableOptions":[{"label":"Send notification email","optionName":"SendEmail","isChecked":true,"isEditable":true,"isDisabled":false}],"nonEditableOptions":[{"label":"Notes and attachments","optionName":"TransferNotesAndAttachments","isChecked":true,"isEditable":false,"isDisabled":false},{"label":"Open activities","optionName":"TransferOpenActivities","isChecked":true,"isEditable":false,"isDisabled":false}],"qualifiedApiName":"Case","ownerFieldLabel":"Case Owner","ownerForeignKeyDomains":[{"name":"User","label":"User","labelPlural":"People","icon":"https://siliconlabs.my.salesforce.com/img/icon/t4v35/standard/user_120.png","color":"65CAE4"},{"name":"Group","label":"Queue","labelPlural":"Queues"}]},"doAccessCheck":false}}]}'
+                        message_data = '{"actions":[{"id":"56925;a","descriptor":"serviceComponent://ui.force.components.controllers.ownerChangeContent.OwnerChangeContentController/ACTION$performOwnerChange","callingDescriptor":"UNKNOWN","params":{"recordIds":["' + key + '"],"newOwnerId":"'+userID+'","changeOwnerOptions":{"editableOptions":[{"label":"Send notification email","optionName":"SendEmail","isChecked":true,"isEditable":true,"isDisabled":false}],"nonEditableOptions":[{"label":"Notes and attachments","optionName":"TransferNotesAndAttachments","isChecked":true,"isEditable":false,"isDisabled":false},{"label":"Open activities","optionName":"TransferOpenActivities","isChecked":true,"isEditable":false,"isDisabled":false}],"qualifiedApiName":"Case","ownerFieldLabel":"Case Owner","ownerForeignKeyDomains":[{"name":"User","label":"User","labelPlural":"People","icon":"https://siliconlabs.my.salesforce.com/img/icon/t4v35/standard/user_120.png","color":"65CAE4"},{"name":"Group","label":"Queue","labelPlural":"Queues"}]},"doAccessCheck":false}}]}'
                         takeData = {
                             "message": message_data,
                             "aura.context": '{"mode":"PROD","fwuid":"kHqYrsGCjDhXliyGcYtIfA","app":"one:one","loaded":{"APPLICATION@markup://one:one":"'+loadID+'"},"dn":[],"globals":{"eswConfigDeveloperName":null,"isVoiceOver":null,"setupAppContextId":null,"density":"VIEW_TWO","srcdoc":null,"appContextId":"06m1M000000KhjGQAS","dynamicTypeSize":null},"uad":true}',
